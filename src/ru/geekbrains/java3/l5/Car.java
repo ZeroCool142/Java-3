5bab0b017302e61d614a4447ffeea99044d1e392
ZeroCool142 <undead142@gmail.com>
2017-04-11 14:16:44 +0300

    Working code

--- src/ru/geekbrains/java3/l5/Car.java
+++ src/ru/geekbrains/java3/l5/Car.java
@@ -1,5 +1,9 @@
 package ru.geekbrains.java3.l5;
 
+import java.util.Comparator;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+
 public class Car implements Runnable {
     private static int CARS_COUNT;
 
@@ -10,6 +14,7 @@ public class Car implements Runnable {
     private Race race;
     private int speed;
     private String name;
+    private final CountDownLatch cdl;
 
     public String getName() {
         return name;
@@ -19,11 +24,12 @@ public class Car implements Runnable {
         return speed;
     }
 
-    public Car(Race race, int speed) {
+    public Car(Race race, int speed, CountDownLatch cdl) {
         this.race = race;
         this.speed = speed;
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
+        this.cdl = cdl;
     }
 
     @Override
@@ -32,12 +38,15 @@ public class Car implements Runnable {
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int)(Math.random() * 800));
             System.out.println(this.name + " готов");
+            MainClass.carPrepared();
+            cdl.await();
         } catch (Exception e) {
             e.printStackTrace();
         }
         for (int i = 0; i < race.getStages().size(); i++) {
             race.getStages().get(i).go(this);
         }
+        MainClass.carPrepared();
     }
 }
 
--- src/ru/geekbrains/java3/l5/MainClass.java
+++ src/ru/geekbrains/java3/l5/MainClass.java
@@ -10,20 +10,40 @@ package ru.geekbrains.java3.l5;
 // Можете корректировать классы(в т.ч. конструктор машин)
 // и добавлять объекты классов из пакета util.concurrent
 
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+
 public class MainClass {
     public static final int CARS_COUNT = 4;
+    private static CountDownLatch cdl = new CountDownLatch(CARS_COUNT);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+
         System.out.println("ВАЖНОЕ ОБЪЯВЕНИЕ >>> Подготовка!!!");
-        Race race = new Race(new Road(60), new Tunnel(), new Road(40));
+        Race race = new Race(new Road(60), new Tunnel(CARS_COUNT), new Road(40));
         Car[] cars = new Car[CARS_COUNT];
         for (int i = 0; i < cars.length; i++) {
-            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10), cdl);
         }
         for (int i = 0; i < cars.length; i++) {
             new Thread(cars[i]).start();
         }
+
+
+        cdl.await();
         System.out.println("ВАЖНОЕ ОБЪЯВЕНИЕ >>> Гонка началась!!!");
+        cdl = new CountDownLatch(CARS_COUNT);
+        cdl.await();
+
         System.out.println("ВАЖНОЕ ОБЪЯВЕНИЕ >>> Гонка закончилась!!!");
+    }
+
+    public static void carPrepared(){
+        cdl.countDown();
+    }
+
+    public static void awaitFinishAll() {
+
     }
 }
